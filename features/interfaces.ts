937bc32e9100caf1e2ae453d8c49b5956052cb2f
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-21 13:10:55 +0330

    041 Functions In Interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,34 +1,20 @@
-interface Vehicle {
-  name: string;
-  year: Date;
-  broken: boolean;
-  summary():string
+interface Reportable {
+  summary(): string;
 }
 
-const oldCivic: Vehicle = {
+const oldCivic = {
   name: 'civic',
   year: new Date(),
   broken: true,
-  summary(){
-    return this.name + this.year + ' is borken:' + this.broken
-  }
+  summary() {
+    return this.name + this.year + ' is borken:' + this.broken;
+  },
 };
 
-const printVehicle = (vehicle: {
-  name: string;
-  year: number;
-  broken: boolean;
-}) => {
-  console.log(`
-    Name: ${vehicle.name}
-    Year: ${vehicle.year}
-    Broken: ${vehicle.broken}
-    `);
-};
 
-const logVehicle = (vehicle: Vehicle) => {
-  console.log(vehicle.summary());
+const printSummary = (item: Reportable) => {
+  console.log(item.summary());
 };
 
 // printVehicle(oldCivic);
-logVehicle(oldCivic);
+printSummary(oldCivic);
