c93cdef7c7c6b6f97c14555901cb96b9e186fdaf
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-21 11:44:49 +0330

    024 More Annotations Around Functions

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,4 +1,6 @@
-let apples: number = 5;
+let apples: number;
+apples = 5;
+
 let speed: string = 'fast';
 let hasName: boolean = true;
 let nothingMuch: null = null;
@@ -27,9 +29,36 @@ let point: { x: number; y: number } = {
 
 point.x = 2;
 
-
 // Function
-const logNumber: (i:number) => void = (i:number)=>{
-  console.log(i)
-  
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x":10, "y":20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates);
+
+// 2) When we declare a variable on one line and initalizate it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+foundWord = false;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+// It's a bad code but just for example
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for(let i = 0; i < numbers.length;i++){
+  if(numbers[i]> 0){
+    numberAboveZero = numbers[i]
+  }
+}
+
