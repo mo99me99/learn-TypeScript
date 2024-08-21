56e8fdfd89c5f83e02cb09eb48ea036b0c49ae74
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-21 10:08:45 +0330

    008 Catching Errors with TypeScript

--- index.ts
+++ index.ts
@@ -2,6 +2,21 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((reqsponse) => {
-  console.log(reqsponse.data);
+  const todo = reqsponse.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is completed? ${completed}
+    `);
 });
