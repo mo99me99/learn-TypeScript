7ae67905ac208d159a5f54f965d6e53ff6ebe36e
mo99me99 <m.m.hosseini1099@gmail.com>
2024-08-21 12:02:35 +0330

    028 Destructuring with Annotations

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,4 +1,35 @@
-const add = (a: number, b: number): number => {
-  return a + b;
+// !!! may forget to return without annotation
+const add = (a: number, b: number) => {
+  a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const forcast = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({date, weather}: { date: Date; weather: string }): void => {
+  console.log(date)
+  console.log(weather)
 };
 
